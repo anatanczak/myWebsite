c41b767046b2e32d5d6102dda30114c6b5cd550f
Ana Viktoriv <anaviktoriv@gmail.com>
2020-03-03 21:29:45 +0100

    create separate file for navbar css, delete hovermenu, add background picture

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-//jshint esversion: 6
-// makes the submenu with cv appear and disappear
-document.getElementById("cv-link").addEventListener("click", ()=> {
-document.getElementById("dropdown-submenu").classList.toggle("submenu-arrow-pressed");
-document.getElementById("arrow-submenu").classList.toggle("arrow-rotate");
-
-});
