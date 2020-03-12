d8fa39722ae89750d091d1eb0393e98998ebae15
Ana Viktoriv <anaviktoriv@gmail.com>
2020-03-12 22:28:49 +0100

    add hhero image to portfolio page

--- portfolio/index.php
+++ portfolio/index.php
@@ -7,11 +7,17 @@ include '../head.php';
 ?>
 
 <body class="body-accueil">
-
+  <div class="main-content-wrapper-portfolio">
+<div class="hero-image-and-title-wrapper-portfolio">
+  <img class="hero-img-portfolio-desktop" src="../assets/images/hero-img-portfolio-desktop.jpg" alt="decorative hero image">
+  <img class="hero-img-portfolio-tablet" src="../assets/images/hero-img-portfolio-ipad-vertical.jpg" alt="decorative hero image">
+  <img class="hero-img-portfolio-mobile" src="../assets/images/hero-img-portfolio-mobile.jpg" alt="decorative hero image">
   <?php if (file_exists('../header.php')) {
   include '../header.php';
   } ?>
-<!-- <script src="../assets/js/script.js"></script> -->
+  </div>
+<script src="../assets/js/script.js"></script>
+</div>
 </body>
 
 </html>
